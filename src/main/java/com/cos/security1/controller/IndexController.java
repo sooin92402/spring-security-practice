8f22cf489786b93c4bcf1e32dcd290f93655e99a
sion <sion@enssel.com>
2022-01-06 17:56:53 +0900

    indexController 생성

--- src/main/java/com/cos/security1/controller/IndexController.java
+++ src/main/java/com/cos/security1/controller/IndexController.java
@@ -3,11 +3,14 @@ package com.cos.security1.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
+@Controller //view 리턴
 public class IndexController {
 	
+	//localhost:8080/
+	//localhost:8080
 	@GetMapping({"","/"})
 	public String index() {
+		//머스테치 
 		return "index";
 	}
 }
