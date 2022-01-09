24027bf7e9fc468b94c969d5bcf191da114f30f0
sooin92402 <sooin92402@gmail.com>
2022-01-09 20:23:09 +0900

    시큐리티 설정 실습

--- src/main/java/com/cos/security1/controller/IndexController.java
+++ src/main/java/com/cos/security1/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.cos.security1.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller //view 리턴
 public class IndexController {
@@ -13,5 +14,36 @@ public class IndexController {
 		// 머스테치 기본폴더 src/main/resources/
 		// 뷰리졸버 설정 : templates (prefix), .mausatche(suffix)생략가능!! 
 		return "index"; //src/main/resources/templates/index.mustache
+	}
+	
+	@GetMapping("/user")
+	public @ResponseBody String user() {
+		return "user";
+	}
+	
+	@GetMapping("/admin")
+	public @ResponseBody String admin() {
+		return "admin";
+	}
+	
+	@GetMapping("/manager")
+	public @ResponseBody String manager() {
+		return "manager";
+	}
+	
+	//스프링시큐리티가 해당 주소를 낚아챔 -SecurityConfig파일작동하지 않음
+	@GetMapping("/login")
+	public @ResponseBody String login() {
+		return "login";
+	}
+	
+	@GetMapping("join")
+	public @ResponseBody String join() {
+		return "join";
+	}
+	
+	@GetMapping("/joinProc")
+	public @ResponseBody String joinProc() {
+		return "회원가입 완료됨!";
 	}
 }
